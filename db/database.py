e91bd9e2bafac8d6e00714c363e51d89940d6185
Simon Rignell <rignellsimon@gmail.com>
2026-03-30 15:50:56 +0200

    sqlalchemy added in requierements + boiler-plats for db

---
+++ db/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine, Column, Interger, String, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+DATABASE_URL = "sqlite:///db/budget.db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
