0a5d6b73f785fffe300d5a38a0e0223a456688ed
Gresa Hoxha <hogr24bj@student.ju.se>
2026-04-03 13:04:09 +0200

    initialized database tables and succesfullt tested register/ and user/

--- app.py
+++ app.py
@@ -2,14 +2,14 @@
 
 from flask import Flask, request, jsonify
 from db.crud import create_user, get_users
-# from db.database import init_db
+from db.database import init_db
 
 
 app = Flask(__name__)
 
 
 # initialize databse tables
-# init_db()
+init_db()
 
 
 # home
