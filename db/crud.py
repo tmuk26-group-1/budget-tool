0043e4abc6ea8f4be7f507b3e85006718c7a0fbc
Albin Törnberg <toernberg@gmail.com>
2026-04-07 16:51:06 +0200

    add try-finally to crud functions
    fixes #38
    fixes #39

--- db/crud.py
+++ db/crud.py
@@ -41,32 +41,37 @@ def get_users():
 def create_transaction(user_id, amount, category_name, date, description = None):
     session = SessionLocal()
 
-    transaction = transactions(user_id=user_id, amount=amount, category_name=category_name, date=date, description=description)
-    session.add(transaction)
-    session.commit()
-    session.close()
+    try:
+        transaction = transactions(user_id=user_id, amount=amount, category_name=category_name, date=date, description=description)
+        session.add(transaction)
+        session.commit()
+    finally:
+        session.close()
 
 def get_transaction(user_id):
     session = SessionLocal()
-
-    trans = session.query(transactions).filter(transactions.user_id == user_id).all()
-    session.close()
-    return trans
+    try:
+        return session.query(transactions).filter(transactions.user_id == user_id).all()
+    finally:
+        session.close()
 
 def create_category(name):
     session = SessionLocal()
 
-    cat = category(name=name)
-    session.add(cat)
-    session.commit()
-    session.close()
+    try:
+        cat = category(name=name)
+        session.add(cat)
+        session.commit()
+    finally:
+        session.close()
 
 def get_category():
     session = SessionLocal()
 
-    cat = session.query(category).all()
-    session.close()
-    return cat
+    try:
+        return session.query(category).all()
+    finally:
+        session.close()
 
 
 
