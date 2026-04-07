eef19092c7b4290de119114dfdbf93bd17734a44
Simon Rignell <rignellsimon@gmail.com>
2026-04-07 16:34:21 +0200

    add transactions and category CRUD and models and tests

--- db/crud.py
+++ db/crud.py
@@ -1,5 +1,5 @@
 from .database import SessionLocal
-from .models import User
+from .models import User, transactions, category
 
 def create_user(email, firstname, lastname, username, password):
     '''
@@ -24,3 +24,39 @@ def get_users():
 
     session.close()
     return users
+
+
+def create_transaction(user_id, amount, category_name, date, description = None):
+    session = SessionLocal()
+
+    transaction = transactions(user_id=user_id, amount=amount, category_name=category_name, date=date, description=description)
+    session.add(transaction)
+    session.commit()
+    session.close()
+
+def get_transaction(user_id):
+    session = SessionLocal()
+
+    trans = session.query(transactions).filter(transactions.user_id == user_id).all()
+    session.close()
+    return trans
+
+def create_category(name):
+    session = SessionLocal()
+
+    cat = category(name=name)
+    session.add(cat)
+    session.commit()
+    session.close()
+
+def get_category():
+    session = SessionLocal()
+
+    cat = session.query(category).all()
+    session.close()
+    return cat
+
+
+
+    
+    
